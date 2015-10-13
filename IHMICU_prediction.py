57b08ae9da1d24df04407a5915eb9641f1308418
robin93 <robinsinghiitd@gmail.com>
2015-10-14 00:59:01 +0530

    added the pulse function

--- IHMICU_prediction.py
+++ IHMICU_prediction.py
@@ -64,29 +64,243 @@ id_label_train = pd.read_csv(os.path.join(cwd,'id_label_train.csv'))
 id_time_labs_train = pd.read_csv(os.path.join(cwd,'id_time_labs_train.csv'))
 id_time_vitals_train = pd.read_csv(os.path.join(cwd,'id_time_vitals_train.csv'))
 
+#print id_age_train.head(10)
 
 #Merging the train files
-id_time_train = pd.merge(id_time_labs_train,id_time_vitals_train,on = ['ID','Time'],how = 'outer')
+id_time_train = pd.merge(id_time_labs_train,id_time_vitals_train,on = ['ID','TIME'],how = 'outer')
 id_age_time_train = pd.merge(id_age_train,id_time_train,on =['ID'],how = 'outer')
 train_df = pd.merge(id_age_time_train,id_label_train,on = ['ID'],how = 'outer')
 
-#print train_df.head(10)
+print train_df.head(10)
 
-index = []
-
-columns = ['first_modified_feature']
+columns = ['Age','Pulse','LABEL']
 
 modified_feature_train_matrix = pd.DataFrame(columns = columns)
 
-#Iterate over rows and print rows
+def score_pulse(pulse):
+	if pulse<=39:
+		return 8
+	elif pulse> 40 and pulse<50:
+		return 5
+	elif pulse>=50 and pulse<100:
+		return 0
+	elif pulse>=100 and pulse<110:
+		return 1
+	elif pulse>=110 and pulse<120:
+		return 5
+	elif pulse>=120 and pulse<140:
+		return 7
+	elif pulse>=140 and pulse <154:
+		return 13
+	else:
+		return 17
+
+def mean_blood_pressure_score(systolic_bp,diastolic_bp):
+	if not isnull(systolic_bp):
+		if not isnull(diastolic_bp):
+			mean_bp = (systolic_bp + diastolic_bp)/2
+			if mean_bp <= 39:
+				return 23
+			elif mean_bp>=40 and mean_bp<60:
+				return 15
+			elif mean_bp>=60 and mean_bp<70:
+				return 7
+			elif mean_bp>=70 and mean_bp<80:
+				return 6
+			elif mean_bp>=80 and mean_bp<100:
+				return 0
+			elif mean_bp>=100 and mean_bp<120:
+				return 4
+			elif mean_bp>=120 and mean_bp<130:
+				return 7
+			elif mean_bp>=130 and mean_bp<140:
+				return 9
+			elif mean_bp>=140:
+				return 10
+
+
 modified_matrix_index = 0
-for index, row in train_df.iterrows():
-    if row['ICU'] == 1:
-    	modified_matrix_index = modified_matrix_index + 1
-    	if len((train_df[(train_df.ID == row['ID']) & (train_df.Time == row['Time'])]).index) < 0:
-    		modified_feature_train_matrix.set_value(modified_matrix_index,'first_modified_feature',0)
-    	else:
-    		modified_feature_train_matrix.set_value(modified_matrix_index,'first_modified_feature',1)
+
+
+Patients_list = pd.unique(train_df.ID)
+for patient in Patients_list:
+ 	if not pd.isnull(patient):
+ 		print 'PatientID',patient
+
+ 		#Age info
+ 		Age_subdata = id_age_train[(id_age_train.ID == patient)]
+ 		Patient_age = int(Age_subdata.AGE)
+ 		#print "age is - ",Patient_age
+
+ 		if int((id_label_train[(id_label_train.ID == patient)]).LABEL) == 1:  #selecting patients who died
+ 			modified_matrix_index = modified_matrix_index + 1
+ 			print modified_matrix_index
+ 			print "This patient died"
+ 			print "Now fetching the subset the data of the patient from the main data"
+			data_sub = train_df[(train_df.ID == patient)]
+			print 'number of measurement of this patient is',len(data_sub)
+
+			
+ 			#EXTRACTING MODIFIED FEATURES FROM NON-ICU DATA of the current patient
+ 			
+ 			non_icu_data = data_sub[(data_sub.ICU == 0)]   #extract data of the patient when he was NOT in ICU
+ 			print 'Pre ICU measurements - ',len(data_sub[(data_sub.ICU == 0)])
+
+ 			#Pulse modified feature for Non-ICU of current patient
+ 			pulse_data_outside_icu = data_sub.V3
+ 			score_list = []
+ 			for measurement in pulse_data_outside_icu:
+ 				if not pd.isnull(measurement):
+ 					score_list.append(score_pulse(int(measurement)))
+ 			if not score_list:
+ 				pulse_non_icu_score = 0
+ 			else:
+ 				pulse_non_icu_score = min(score_list)
+
+ 			#Mean BP modified feature for Non-ICU of current patient
+ 			columns_of_interest = ['V1','V2']
+
+
+ 			#Temperature modified feature for Non-ICU of current patient
+ 			#Respiratory Rate modified feature for Non-ICU of current patient
+ 			#Partial Pressure of Oxygen modified feature for Non-ICU of current patient
+ 			#AaDO2 modified feature for Non-ICU of current patient
+ 			#Hematocrit modified feature for Non-ICU of current patient
+ 			#WBC count modified feature for Non-ICU of current patient
+ 			#Serum Creatinine modified feature for Non-ICU of current patient
+ 			#Urine Output modified feature for Non-ICU of current patient
+ 			#Serun BUN modified feature for Non-ICU of current patient
+ 			#Serum Na modified feature for Non-ICU of current patient
+ 			#Serum Albumin modified feature for Non-ICU of current patient
+ 			#Serum Bilirubin modified feature for Non-ICU of current patient
+ 			#Serum Glucose modified feature for Non-ICU of current patient
+ 			
+
+
+ 			# #Appending the modified non-ICU features for this patient
+ 			modified_feature_train_matrix.set_value(modified_matrix_index,'Age',Patient_age)
+ 			modified_feature_train_matrix.set_value(modified_matrix_index,'Pulse',pulse_non_icu_score)
+ 			modified_feature_train_matrix.set_value(modified_matrix_index,'LABEL',0)
+
+
+
+ 			#EXTRACTING MODIFIED FEATURES FROM ICU DATA OF THE CURRENT PATIENT
+ 			modified_matrix_index = modified_matrix_index +1
+ 			print modified_matrix_index
+ 			icu_data = data_sub[(data_sub.ICU == 1)]       #extract data of the patient when he was in IC
+ 			print 'ICU measurements - ',len(data_sub[(data_sub.ICU == 1)])
+
+ 			#Pulse modified feature for ICU of current patient
+ 			pulse_data_inside_icu = data_sub.V3
+ 			score_list = []
+ 			for measurement in pulse_data_inside_icu:
+ 				if not pd.isnull(measurement):
+ 					score_list.append(score_pulse(int(measurement)))
+ 			if not score_list:
+ 				pulse_icu_score = pulse_non_icu_score
+ 			else:
+ 				pulse_icu_score = max(score_list)
+
+ 			#Mean BP modified feature for ICU of current patient
+ 			#Temperature modified feature for ICU of current patient
+ 			#Respiratory Rate modified feature for ICU of current patient
+ 			#Partial Pressure of Oxygen modified feature for ICU of current patient
+ 			#AaDO2 modified feature for ICU of current patient
+ 			#Hematocrit modified feature for ICU of current patient
+ 			#WBC count modified feature for ICU of current patient
+ 			#Serum Creatinine modified feature for ICU of current patient
+ 			#Urine Output modified feature for ICU of current patient
+ 			#Serun BUN modified feature for ICU of current patient
+ 			#Serum Na modified feature for ICU of current patient
+ 			#Serum Albumin modified feature for ICU of current patient
+ 			#Serum Bilirubin modified feature for ICU of current patient
+ 			#Serum Glucose modified feature for ICU of current patient
+
+ 			#Appending the modified non-ICU features for this patient
+ 			modified_feature_train_matrix.set_value(modified_matrix_index,'Age',Patient_age)
+ 			modified_feature_train_matrix.set_value(modified_matrix_index,'Pulse',pulse_icu_score)
+ 			modified_feature_train_matrix.set_value(modified_matrix_index,'LABEL',1)
+
+ 		else:   #selecting patients who survived
+ 			print "This patient did not die"
+ 			print "Now fetching the subset the data of the patient from the main data"
+ 			data_sub = train_df[(train_df.ID == patient)]
+ 			print 'number of measurement of this patient is',len(data_sub)
+ 			print 'Pre ICU measurements - ',len(data_sub[(data_sub.ICU == 0)])
+ 			
+ 			#EXTRACTING MODIFIED FEATURES FROM NON-ICU DATA of the current patient
+ 			modified_matrix_index = modified_matrix_index + 1
+ 			non_icu_data = data_sub[(data_sub.ICU == 0)]   #extract data of the patient when he was NOT in ICU
+ 			print 'Pre ICU measurements - ',len(data_sub[(data_sub.ICU == 0)])
+
+ 			#Pulse modified feature for Non-ICU of current patient
+ 			pulse_data_outside_icu = data_sub.V3
+ 			score_list = []
+ 			for measurement in pulse_data_outside_icu:
+ 				if not pd.isnull(measurement):
+ 					score_list.append(score_pulse(int(measurement)))
+ 			if not score_list:
+ 				pulse_non_icu_score = 0
+ 			else:
+ 				pulse_non_icu_score = min(score_list)
+
+ 			#Mean BP modified feature for Non-ICU of current patient
+ 			#Temperature modified feature for Non-ICU of current patient
+ 			#Respiratory Rate modified feature for Non-ICU of current patient
+ 			#Partial Pressure of Oxygen modified feature for Non-ICU of current patient
+ 			#AaDO2 modified feature for Non-ICU of current patient
+ 			#Hematocrit modified feature for Non-ICU of current patient
+ 			#WBC count modified feature for Non-ICU of current patient
+ 			#Serum Creatinine modified feature for Non-ICU of current patient
+ 			#Urine Output modified feature for Non-ICU of current patient
+ 			#Serun BUN modified feature for Non-ICU of current patient
+ 			#Serum Na modified feature for Non-ICU of current patient
+ 			#Serum Albumin modified feature for Non-ICU of current patient
+ 			#Serum Bilirubin modified feature for Non-ICU of current patient
+ 			#Serum Glucose modified feature for Non-ICU of current patient
+
+ 			#Appending the modified non-ICU features for this patient
+ 			modified_feature_train_matrix.set_value(modified_matrix_index,'Age',Patient_age)
+ 			modified_feature_train_matrix.set_value(modified_matrix_index,'Pulse',pulse_non_icu_score)
+ 			modified_feature_train_matrix.set_value(modified_matrix_index,'LABEL',0)
+
+ 			#EXTRACTING MODIFIED FEATURES FROM ICU DATA OF THE CURRENT PATIENT
+ 			modified_matrix_index = modified_matrix_index + 1
+ 			icu_data = data_sub[(data_sub.ICU == 1)]       #extract data of the patient when he was in IC
+ 			print 'ICU measurements - ',len(data_sub[(data_sub.ICU == 1)])
+
+ 			#Pulse modified feature for ICU of current patient
+ 			pulse_data_inside_icu = data_sub.V3
+ 			score_list = []
+ 			for measurement in pulse_data_inside_icu:
+ 				if not pd.isnull(measurement):
+ 					score_list.append(score_pulse(int(measurement)))
+ 			if not score_list:
+ 				pulse_icu_score = pulse_non_icu_score
+ 			else:
+ 				pulse_icu_score = max(score_list)
+
+ 			#Mean BP modified feature for ICU of current patient
+ 			#Temperature modified feature for ICU of current patient
+ 			#Respiratory Rate modified feature for ICU of current patient
+ 			#Partial Pressure of Oxygen modified feature for ICU of current patient
+ 			#AaDO2 modified feature for ICU of current patient
+ 			#Hematocrit modified feature for ICU of current patient
+ 			#WBC count modified feature for ICU of current patient
+ 			#Serum Creatinine modified feature for ICU of current patient
+ 			#Urine Output modified feature for ICU of current patient
+ 			#Serun BUN modified feature for ICU of current patient
+ 			#Serum Na modified feature for ICU of current patient
+ 			#Serum Albumin modified feature for ICU of current patient
+ 			#Serum Bilirubin modified feature for ICU of current patient
+ 			#Serum Glucose modified feature for ICU of current patient
+
+ 			#Appending the modified non-ICU features for this patient
+ 			modified_feature_train_matrix.set_value(modified_matrix_index,'Age',Patient_age)
+ 			modified_feature_train_matrix.set_value(modified_matrix_index,'Pulse',pulse_icu_score)
+ 			modified_feature_train_matrix.set_value(modified_matrix_index,'LABEL',0)
+
+
 
 print modified_feature_train_matrix
 
@@ -161,15 +375,3 @@ print modified_feature_train_matrix
 
 # #Output_file = pd.read_csv(os.path.join(cwd,'output_prediction.csv'))
 # #check_file = pd.merge(Output_file,id,on = ['ID','Time'],how = 'outer')
-
-
-
-
-
-
-
-
-
-
-
- 
